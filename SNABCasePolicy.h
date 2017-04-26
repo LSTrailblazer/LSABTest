4c724177d2b37b0d58641c706df3f57778342757
LSTrailblazer <407557715@qq.com>
2017-04-26 10:27:25 +0800

    Add files via upload

---
+++ SNABCasePolicy.h
@@ -0,0 +1,23 @@
+//
+//  SNABCasePolicy.h
+//  SNABTest
+//
+//  Created by laizw on 2017/4/6.
+//  Copyright © 2017年 sina. All rights reserved.
+//
+
+@import Foundation;
+
+@protocol SNABCasePolicy <NSObject>
+
+- (NSString *)testName;
+- (NSString *)caseName;
+
+@end
+
+@interface SNABCasePolicy : NSObject <SNABCasePolicy>
+
+@property (nonatomic, copy) NSString *testName;
+@property (nonatomic, copy) NSString *caseName;
+
+@end
